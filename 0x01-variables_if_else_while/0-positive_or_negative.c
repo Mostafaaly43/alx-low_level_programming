cf70d0bae72e8cfd5b96ef06c4028c614efef422
Mostafaaly43 <mustafaaly43@gmail.com>
2023-07-16 11:10:04 -0700

    positive negative task try2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,10 @@
 #include <stdlib.h> 
-#include <time.h>
-#include <stdio.h> 
+ #include <time.h> 
+ #include <stdio.h> 
   
  /** 
-  * main - assigns a random number to int n everytime 
-  * it executes, and prints it 
+  * main - Entry point 
+  * 
   * Return: Always 0 (Success) 
   */ 
  int main(void) 
@@ -13,11 +13,13 @@
   
          srand(time(0)); 
          n = rand() - RAND_MAX / 2; 
-         if (n > 0) 
-                 printf("%d is positive\n", n); 
-         else if (n == 0) 
+  
+         if (n == 0) 
                  printf("%d is zero\n", n); 
          else if (n < 0) 
                  printf("%d is negative\n", n); 
+         else 
+                 printf("%d is positive\n", n); 
+  
          return (0); 
  }
